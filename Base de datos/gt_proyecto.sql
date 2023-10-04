4e31b3625df20b452f25f989a117c5ee00ecde6c
Christiangabcrespo <christiangcrespo2295@gmail.com>
2023-10-04 16:55:57 -0300

    BD

--- Base de datos/gt_proyecto.sql
+++ Base de datos/gt_proyecto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-10-2023 a las 09:36:02
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 04-10-2023 a las 21:54:52
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,7 @@ CREATE TABLE `tareas` (
   `id` int(11) NOT NULL,
   `tarea` varchar(255) NOT NULL,
   `id_usuario` int(11) DEFAULT NULL,
-  `fecha` datetime DEFAULT NULL
+  `fecha` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -39,9 +39,10 @@ CREATE TABLE `tareas` (
 --
 
 INSERT INTO `tareas` (`id`, `tarea`, `id_usuario`, `fecha`) VALUES
-(4, 'dormir una siesta ', 1, '2023-10-16 00:00:00'),
-(5, 'hacer pizza ', 1, '2023-10-24 00:00:00'),
-(6, 'Hacer un cuento', 3, '2023-10-09 00:00:00');
+(4, 'dormir una siesta ', 1, '2023-10-16'),
+(6, 'Hacer un cuento', 3, '2023-10-09'),
+(15, 'terminar lo de proyecto', 3, '2023-10-04'),
+(24, 'terminar lo de proyecto', 1, '2023-10-10');
 
 -- --------------------------------------------------------
 
@@ -90,7 +91,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `tareas`
 --
 ALTER TABLE `tareas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
